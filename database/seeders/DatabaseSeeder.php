66273699068f866ddbaea4877dabdb4abe4bdd47
Mikołaj <mikolajkocik@onet.pl>
2025-12-22 18:28:57 +0100

    Add models with relationship and product controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::create(['name' => 'Elektronika']);
+        Product::factory(10)->create();
+
         // User::factory(10)->create();
 
         User::factory()->create([
