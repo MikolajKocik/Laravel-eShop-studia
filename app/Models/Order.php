15c5ded449ba4150de7651ecb641922720e9210d
Mikołaj <mikolajkocik@onet.pl>
2026-01-01 17:08:20 +0100

    Update: order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['user_id', 'total_price', 'status', 'product_id'];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
